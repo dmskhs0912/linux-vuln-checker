fe35805cef84334eb97ea45d6ed9f5e8648679db
dmskhs0912 <dmskhs0912@gmail.com>
2025-06-01 16:56:13 +0900

    refactor(utils): 각 함수에서 printf의 표준 출력을 /dev/null로 리다이렉트

--- lib/utils.sh
+++ lib/utils.sh
@@ -8,7 +8,7 @@
 # 인자로는 “항목 식별자 + 메시지” 형태의 문자열을 받는다
 result_pass() {
   local message="$1"
-  printf "\e[32m[PASS]\e[0m %s\n" "$message"
+  printf "\e[32m[PASS]\e[0m %s\n" "$message" > /dev/null
   # 전역 배열에 메시지를 추가
   PASSED+=("$message")
   TOTAL_RESULT+=("$message")
@@ -17,7 +17,7 @@ result_pass() {
 # FAIL 결과 처리 함수
 result_fail() {
   local message="$1"
-  printf "\e[31m[FAIL]\e[0m %s\n" "$message"
+  printf "\e[31m[FAIL]\e[0m %s\n" "$message" > /dev/null
   FAILED+=("$message")
   TOTAL_RESULT+=("$message")
 }
