6a2f2babd898eb5fb01a1836a4290f52bdc25ed3
dmskhs0912 <dmskhs0912@gmail.com>
2025-06-03 21:52:15 +0900

    feat: U-09 검사 함수 구현

--- modules/file_dir_management.sh
+++ modules/file_dir_management.sh
@@ -63,7 +63,7 @@ check_passwd_owner() {
 
 }
 
-# U-07 /etc/shadow 파일 소유자 및 권한 설정
+# U-08 /etc/shadow 파일 소유자 및 권한 설정
 check_shadow_owner() {
     log_check_start "U-08" "for a owner and a permission of /etc/shadow file"
 
@@ -82,7 +82,27 @@ check_shadow_owner() {
     fi
 }
 
+# U-09 /etc/hosts 파일 소유자 및 권한 설정
+check_hosts_owner() {
+    log_check_start "U-09" "for a owner and a permission of /etc/hosts file"
+
+    local owner=$(stat -c "%U" "/etc/hosts")
+    local perm=$(stat -c "%a" "/etc/hosts")
+
+    if [[ "$owner" != "root" ]]; then
+        result_fail "U-09 /etc/hosts 파일의 소유자가 root가 아닌 ${$owner}로 되어 있음 (취약)"
+        return
+    fi
+
+    if [[ "$perm" -le 600 ]]; then
+        result_pass "U-09 /etc/hosts 소유자 및 권한이 적절하게 설정되어 있음 (양호)"
+    else
+        result_fail "U-09 /etc/hosts 파일의 권한이 600 이하가 아님 (취약)"
+    fi
+}
+
 check_root_path
 check_nouser_files
 check_passwd_owner
 check_shadow_owner
+check_hosts_owner
